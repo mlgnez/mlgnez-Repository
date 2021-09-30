22d2ea164e59d6b499150a3b75e02a4b02deffc7
mlgnez <91499205+mlgnez@users.noreply.github.com>
2021-09-30 15:12:36 -0500

    Update scripting.js
    added three tries then lock out functionality

--- ui/scripting.js
+++ ui/scripting.js
@@ -1,30 +1,47 @@
 let amountofletters;
+var amountoftries = 0
 function verifyPress()
 {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    amountofletters = Math.floor(Math.random() * 15);
-    var alphabetResult = ""
-    for (let i = 1; i <= amountofletters; i++) {
-        alphabetResult += alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
-    document.getElementById("usernameid").innerText = "Username: " + alphabetResult;
+    if(amountoftries != 3){
+        const alphabet = "abcdefghijklmnopqrstuvwxyz";
+        amountofletters = Math.floor(Math.random() * 15);
+        var alphabetResult = ""
+        for (let i = 1; i <= amountofletters; i++) {
+                alphabetResult += alphabet[Math.floor(Math.random() * alphabet.length)];  
+        }
+        document.getElementById("usernameid").innerText = "Username: " + alphabetResult;
+        }
+        if(amountoftries == 3){
+            console.log("maximum 3 tries")
+            document.getElementById("failedlogin").innerText = "Maximum of 3 login failures. Try again in 24 hours."
+        }
     }
 function loginPress()
 {
-    document.getElementById("failedlogin").innerText = "Login Failed, Did you enter the right login?"
+    if(amountoftries != 3){
+        document.getElementById("failedlogin").innerText = "Login Failed, Did you enter the right login?"
+        amountoftries = amountoftries + 1
+        console.log(amountoftries)
+    }
+    if(amountoftries == 3){
+        console.log("maximum 3 tries")
+        document.getElementById("failedlogin").innerText = "Maximum of 3 login failures. Try again in 24 hours."
+    }
 }
 function passPress()
 {
-   let amountofletters2
-    amountofletters2 = Math.floor(Math.random() * 15);
-    const periods = ".............."
-    var periodsresult = ""
-    for (let i = 1; i <= amountofletters2; i++) {
-        periodsresult += periods[Math.floor(Math.random() * periods.length)];
+    if(amountoftries != 3){
+        let amountofletters2
+        amountofletters2 = Math.floor(Math.random() * 15);
+        const periods = ".............."
+        var periodsresult = ""
+        for (let i = 1; i <= amountofletters2; i++) {
+            periodsresult += periods[Math.floor(Math.random() * periods.length)];
+        }
+        if(amountoftries == 3){
+            console.log("maximum 3 tries")
+            document.getElementById("failedlogin").innerText = "Maximum of 3 login failures. Try again in 24 hours."
+        }
+        document.getElementById("passwordid").innerText = "Password: " + periodsresult
     }
-    
-    document.getElementById("passwordid").innerText = "Password: " + periodsresult
 }
-// code for firing document out of a function. if (document.addEventListener("DOMContentLoaded", function() {}));
-
-//when you put in correct name, you then have to put in password.
